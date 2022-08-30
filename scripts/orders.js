f406b0a08434defd736fbb2097c95c98ca18024c
Holden D Reed <holdendreed@hotmail.com>
2022-08-29 19:38:01 -0500

    Added JavaScript

--- scripts/orders.js
+++ scripts/orders.js
@@ -13,12 +13,34 @@ const orders = [
       instructions: "half green peppers"
     }
   ]
+
+
+
+
+
+  const getNewOrderId = () => {
+    let highestOrderId = orders.sort((a, b) => b.id - a.id)[0].id
+    return highestOrderId + 1
+  }
+  console.log(getNewOrderId())
   
+  export const addNewOrder = (order) => {
+    const newId = getNewOrderId()
+    // Add logic to give the order the property id, which is equal to newId
+    order.id = newId
+    // Add logic to add the order object to the orders array
+    orders.push(order)
+    console.log(orders)
+    // The below line creates and dispatches a new custom event titled "stateChanged".
+    // We will still need to add a listener for this event.
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+  }
+
+
+
+
   export const getOrders = () => {
     let ordersData = orders.map(order => ({...order}))
     return ordersData
     // Add logic here to return a copy of your orders
   }
-export const addNewOrder = () => {
-
-}
